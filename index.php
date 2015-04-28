bf4f5bf42cdf68da37414ac1df548fe0a37f1b4d
krzyurb <krzyurb@gmail.com>
2015-04-28 22:51:54 +0200

    Improved index.php

--- index.php
+++ index.php
@@ -6,22 +6,16 @@ switch($_GET['task']){
 	case "main":
 		include 'controller/main.php';
 		$ob = new MainController();
-		if($_GET['action']=='index')
-			$ob->index();
-		if($_GET['action']=='about')
-			$ob->about();
-		if($_GET['action']=='find')
-			$ob->find();
+			$ob->$_GET['action']();
+
 	break;
 	
 	case "user":
 		include 'controller/user.php';
 		$ob = new UserController();
-		if($_GET['action']=='login')
-			$ob->login();
-		if($_GET['action']=='register')
-			$ob->register();
+		$ob->$_GET['action']();
 	break;
+	
 }
 
 ?>
