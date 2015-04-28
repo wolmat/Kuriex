fc61d4658dff384ac014162d799e06ff4c127b98
Dawid Paluch <paluchdd@gmail.com>
2015-04-28 14:47:59 +0200

    Unify headings in blocks
    - Fix typos

--- templates/main/index.php
+++ templates/main/index.php
@@ -19,15 +19,15 @@
 	<div class="container">
 		<div class="row">
 			<div class="one-half column" style="margin-top:3%">
-				<h1>Zaufalo nam juz</h1>
-				<h2>Przekazalismy</h1>
-				<h3>W pocie czoła pracuje </h3>
+				<h2>Zaufało nam już</h2>
+				<h2>Przekazalismy</h2>
+				<h2>W pocie czoła pracuje </h2>
 			</div>
 			
 			<div class="one-half column" style="margin-top:3%; color: #f07830">
-				<h1>5 milionów klientów</h1>
-				<h2>6.5 miliona paczek</h1>
-				<h3>4432 pracowników</h3>
+				<h2>5 milionów klientów</h2>
+				<h2>6.5 miliona paczek</h2>
+				<h2>4432 pracowników</h2>
 			</div>
 			
 			<div class="one-half column" style="margin-top:3%">
@@ -42,7 +42,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="one-full column" style="margin-top:5%">
-				<h1>Dzialamy juz w calej polsce!</h1>
+				<h1>Działamy już w całej Polsce!</h1>
 			</div>
 		</div>
 			
@@ -52,10 +52,10 @@
 			</div>
 			
 			<div class="one-half column" style="margin-top:5%">
-				<h5>Mamy filie w <span style="color: #f07830">kazdym</span> województwie</h5>
-				<h4>Dzialamy w <span style="color: #f07830">243</span> miastach</h4>
-				<h3>Dotrzemy <span style="color: #f07830">wszedzie</span>!</h3>
-				<h2>Bo dla nas granice </br><span style="color: #f07830">nie istnieja</span></h2>
+				<h4>Filia w <span style="color: #f07830">każdym</span> województwie</h4>
+				<h4>Działamy w <span style="color: #f07830">243</span> miastach</h4>
+				<h4>Dotrzemy <span style="color: #f07830">wszędzie</span>!</h4>
+				<h4>Bo dla nas granice <span style="color: #f07830">nie istnieją</span></h4>
 			</div>
 		</div>
 	</div>
@@ -65,15 +65,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="one-full column" style="margin-top:5%">
-				<h2>Zlokalizuj swoja paczke!</h2>
-				<h4>Podaj kod ktory otrzymales emailem!</h4>
+				<h2>Zlokalizuj swoją paczkę!</h2>
+				<h4>Podaj kod, który otrzymałeś emailem!</h4>
 				
 				  <form method="post">
-					ID przesylki: </br><input type="text" name="id"><br>
+					ID przesyłki: </br><input type="text" name="id"><br>
 					<input style="margin auto" type="submit" value="Szukaj">
 				  </form>
-				<h5>Nasze specjalnie przygotowane serwery zrobia wszystko zebys myslal ze nie mamy na ciebie <span style="color: #f07830">wyjebane</span></h5>
-				<h6>szmato</h6>
+				<h5>Nasze specjalnie przygotowane serwery zrobia wszystko żebys myslal, że nie mamy na ciebie <span style="color: #f07830">wyjebane</span></h5>
 			</div>
 			
 		</div>
