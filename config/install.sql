70f7a5c54d5daf963a49740a86277c41e26f1294
dave <paluchdd@gmail.com>
2015-05-30 14:50:27 +0000

    Delete existing tables in sql script

--- config/install.sql
+++ config/install.sql
@@ -2,3 +2,8 @@ CREATE DATABASE IF NOT EXISTS `kuriex`;
 
 GRANT ALL ON `kuriex`.* TO `kuriex`@`localhost`;
 SET PASSWORD FOR 'kuriex'@'localhost' = PASSWORD('kuriex');
+
+USE kuriex;
+
+DROP TABLE IF EXISTS kurier, dostawca, kurier_katprawajazdy,
+    dostawca_katprawajazdy, filia, obszar, rejon;
