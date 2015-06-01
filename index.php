106a89c784e477bf7e11eef49b41c180d756a526
dave <paluchdd@gmail.com>
2015-06-01 09:11:03 +0000

    Refactor routing code in index

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
+$file = 'controller/main.php';
+$controller_class = 'MainController';
+$action = 'index';
 
-
-switch($_GET['task']){
-	
-	case "main":
-		include 'controller/main.php';
-		$ob = new MainController();
-		$ob->$_GET['action']();
-	break;
-	
-	case "user":
-		include 'controller/user.php';
-		$ob = new UserController();
-		$ob->$_GET['action']();
-	break;
-	
+// when controller or its action not found
+// redirect to main controller, index action
+if(file_exists('controller/'.$_GET['task'].'.php')){
+    require_once('controller/'.$_GET['task'].'.php');
+   
+    if(class_exists(ucfirst($_GET['task']).'Controller')
+        && method_exists(ucfirst($_GET['task']).'Controller', $_GET['action'])){
+        
+        $file = 'controller/'.$_GET['task'].'.php';
+        $controller_class = ucfirst($_GET['task']).'Controller';
+        $action = $_GET['action'];
+    }
 }
+
+require_once($file);
+$controller = new $controller_class();
+$controller->$action();
 
 ?>
