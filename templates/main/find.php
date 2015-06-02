f30d9cd00a2ef4eaf4d980a593a9768425d0d89d
krzyurb <krzyurb@gmail.com>
2015-06-02 14:52:51 +0200

    Delete one-half rows

--- templates/main/find.php
+++ templates/main/find.php
@@ -1,8 +1,7 @@
 <?php include 'templates/header.php'; ?>
 
 	<div class="container">
-		<div class="row">
-			<div class="one-half column" style="margin-top:5%">
+
 				<h3>Znajdz paczke</h3>
 				<form action="find" method="post">
 					<table>
@@ -17,8 +16,6 @@
                     </table>
 				    <input style="margin auto" type="submit" value="Znajdz">
 				</form>
-			</div>
-		</div>
 	</div>
 
 <?php include 'templates/footer.php'; ?>
