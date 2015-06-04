57896b79dfad6199c39bf19f652752a562e44401
krzyurb <krzyurb@gmail.com>
2015-06-04 23:46:30 +0200

    Delete some columns

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -9,21 +9,14 @@
         <h5> Oczekujące zlecenia </h5>
             
                     <table id="order-table">
-                        <thead>
                         <tr>
                             <td>Id </td>
                             <td>Opis </td>
-                            <td>Rodzaj płatności </td>
-                            <td>Status </td>
-                            <td>Pesel nadawcy </td>
                         </tr>
                         <?php foreach($this->get('orders') as $order) { ?>
                         <tr>
                             <td><?php echo $order['id_zlecenia']; ?></td>
                             <td><?php echo $order['opis']; ?></td>
-                            <td><?php echo $order['rodzaj_platnosci']; ?></td>
-                            <td><?php echo $order['status']; ?></td>
-                            <td><?php echo $order['pesel_nadawcy']; ?></td>
                         </tr>
                         <?php } ?>
                         
