b7edeae6e93d2b54603cfcd5939339d0da30fb0e
dave <paluchdd@gmail.com>
2015-06-04 19:27:34 +0200

    Update font to PT Sans

--- templates/header.php
+++ templates/header.php
@@ -8,8 +8,8 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
-
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+  
   <!-- include all css stylesheets -->
   <?php foreach(glob('css/*.css') as $css): ?>
     <link rel="stylesheet" href="<?php echo $css; ?>">
