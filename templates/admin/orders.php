a07f043472e1c5c5b393e78dc13334f705ec7655
dave <paluchdd@gmail.com>
2015-06-07 21:50:15 +0200

    Update orders template

--- templates/admin/orders.php
+++ templates/admin/orders.php
@@ -1,50 +1,69 @@
 <?php include 'templates/header.php'; ?>
 <?php include('header.php'); ?>
-	<div class="container">
 
-				<h3>Lista zleceń </h3>
+<div class="container">
+    <h3>Lista zleceń</h3>
+    <form action="orders" method="post">
+        <table id="order-table">
 
-				<form action="orders" method="post">
-					<table id="order-table">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Opis</th>
+                <th>Rodzaj płatności</th>
+                <th>Status</th>
+                <th>Nadawca</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><input type="text" name="id_zlecenia"></td>
+                    <td><input type="text" name="opis"></td>
+                    <td><input type="text" name="rodzaj_platnosci"></td>
+                    <td><input type="text" name="status"></td>
+                    <td><input type="text" name="pesel_nadawcy"></td>
+                </tr>
+                <?php foreach($this->get('orders') as $order): ?>
+                <tr class="order">
+                    <td><?php echo $order->id_zlecenia; ?></td>
+                    <td><?php echo $order->opis; ?></td>
+                    <td><?php echo $order->rodzaj_platnosci; ?></td>
+                    <td><?php echo $order->status; ?></td>
+                    <td><?php echo $order->nadawca; ?></td>
+                </tr>
+                <tr class="package-tr">
+                    <td colspan="5">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Opis</th>
+                                    <th>Status</th>
+                                    <th>Pesel dostawcy</th>
+                                    <th>Pesel kuriera</th>
+                                    <th>Pesel odbiorcy</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($order->przesylki as $delivery): ?>
+                                <tr>
+                                    <td> <?php echo $delivery['id_przesylki']; ?> </td>
+                                    <td> <?php echo $delivery['opis']; ?> </td>
+                                    <td> <?php echo $delivery['status']; ?> </td>
+                                    <td> <?php echo $delivery['pesel_dostawcy']; ?> </td>
+                                    <td> <?php echo $delivery['pesel_kuriera']; ?> </td>
+                                    <td> <?php echo $delivery['pesel_odbiorcy']; ?> </td>
+                                </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <input style="margin auto" type="submit" value="Znajdz">
+    </form>
+</div>
 
-                        <thead>
-                        <tr>
-                            <td>Id </td>
-                            <td>Opis </td>
-                            <td>Rodzaj płatności </td>
-                            <td>Status </td>
-                            <td>Pesel nadawcy </td>
-                        </tr>
-                        </thead>
-                        <tr>
-                            <td><input type="text" name="id_zlecenia"></td>
-                            <td><input type="text" name="opis"></td>
-                            <td><input type="text" name="rodzaj_platnosci"></td>
-                            <td><input type="text" name="status"></td>
-                            <td><input type="text" name="pesel_nadawcy"></td>
-                        </tr>
-                        <?php foreach($this->get('orders') as $order) { ?>
-                        <tr class="order">
-                            <td><?php echo $order->id_zlecenia; ?></td>
-                            <td><?php echo $order->opis; ?></td>
-                            <td><?php echo $order->rodzaj_platnosci; ?></td>
-                            <td><?php echo $order->status; ?></td>
-                            <td><?php echo $order->nadawca; ?></td>
-                        </tr>
-                        <?php foreach($order->przesylki as $delivery) { ?>
-                            <tr class="package">
-                                <td> <?php echo $delivery['id_przesylki']; ?> </td>    
-                                <td> <?php echo $delivery['opis']; ?> </td>    
-                                <td> <?php echo $delivery['status']; ?> </td>    
-                                <td> <?php echo $delivery['pesel_dostawcy']; ?> </td>    
-                                <td> <?php echo $delivery['pesel_kuriera']; ?> </td>    
-                                <td> <?php echo $delivery['pesel_odbiorcy']; ?> </td>    
-                            </tr>                        
-                        <?php } } ?>
-                        
-                    </table>
-				    <input style="margin auto" type="submit" value="Znajdz">
-				</form>
-	</div>
-
-<?php include 'templates/footer.php'; 
+<?php include 'templates/footer.php'; ?>
