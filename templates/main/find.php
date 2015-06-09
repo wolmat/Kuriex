6d7e20bd9e94e938d5ec98063b8b65574f75e21e
krzyurb <krzyurb@gmail.com>
2015-06-09 22:07:19 +0200

    Change find page template

--- templates/main/find.php
+++ templates/main/find.php
@@ -1,15 +1,18 @@
 <?php include 'templates/header.php'; ?>
 
-	<div class="container">
-
-				<h3>Znajdz paczkę</h3>
-				<form action="find" method="post">
-
-                    <h5> ID paczki: </h5>
-                    <input type="text" name="id_przesylki">
-
-				    <input style="margin auto" type="submit" value="Znajdz">
-				</form>
-	</div>
+<div id="block3" style="text-align: center;">
+  <div id="container">
+    <div class="row">
+      <div class="one-full column">
+        <h2>Zlokalizuj swoją paczkę!</h2>
+        <h4>Podaj kod, który otrzymałeś emailem!</h4>
+        <form action="find" method="post">
+          <input style="color: #f07830; font-weight: bold" type="text" name="id_przesylki">
+          <input style="background: #fff; color: #f07830" type="submit" value="Szukaj">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include 'templates/footer.php'; ?>
