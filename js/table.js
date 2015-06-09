cdc9e5b23f93f8cbf912228b3f2a64af1b6a24b4
dave <paluchdd@gmail.com>
2015-06-09 12:05:02 +0200

    FadeIn/FadeOut CRUD icons on customer hover

--- js/table.js
+++ js/table.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     $('tr:odd').addClass('odd');
     $('tr:even').addClass('even');
+
+    $('.customer').on({
+        mouseenter: function(){
+            $(this).find('.crud').fadeIn();
+        },
+        mouseleave: function(){
+            $(this).find('.crud').fadeOut();
+        }
+    });
 });
