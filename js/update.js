12015f6d5cc0a15afb208c8067c316c826157e4d
dave <paluchdd@gmail.com>
2015-06-11 23:45:04 +0200

    Add jquery customer deletion

--- js/update.js
+++ js/update.js
@@ -16,6 +16,29 @@ $(document).ready(function(){
     });
 
     $('.delete').on('click', function(){
+        var tr = $(this).parent().parent();
+        var data = {
+            delete: tr.find('td:first-child').text()
+        };
+        $.ajax({
+            url: 'customers',
+            type: 'post',
+            data: data,
+            success: function(result){
+                var message = $('.message');
+                if(!$(message).length){
+                    message = $(result).find('.message');
+                    $(message).hide();
+                    $('h3').after(message);
+                } else {
+                    $(message).hide().replaceWith($(result).find('.message'));
+                }
+                $(message).fadeIn();
+                if($(message).hasClass('info')){
+                    tr.fadeOut();
+                }
+            }
+        });
         return false;
     });
 
