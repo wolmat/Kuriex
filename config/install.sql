8ccba977a3ebc1ccc19302349113bf64e315a34b
davpal <paluchdd@gmail.com>
2015-06-20 23:55:50 +0200

    Set names utf8 in sql install script

--- config/install.sql
+++ config/install.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `kuriex`
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+SET NAMES utf8;
 
 GRANT ALL ON `kuriex`.* TO `kuriex`@`localhost`;
 SET PASSWORD FOR 'kuriex'@'localhost' = PASSWORD('kuriex');
