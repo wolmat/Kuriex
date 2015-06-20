53582644cfd6e7d1915c76737d6b25f155daa8c9
davpal <paluchdd@gmail.com>
2015-06-20 14:45:26 +0200

    Update customers view message handling

--- templates/admin/customers.php
+++ templates/admin/customers.php
@@ -8,7 +8,17 @@
                          <?php echo count($data['customers']); ?>
                     </span>
                 </h3>
-                <?php echo $data['message']; ?>
+                <?php if(isset($data['message'])): ?>
+                    <div class="message <?php echo $data['message-type'] ?>">
+                        <?php echo $data['message']; ?>
+                        <?php
+                            if(isset($data['fields']))
+                                foreach($data['fields'] as $field){
+                                    echo '<span class="field">'.$field.'</span>';
+                                }
+                        ?>
+                    </div>
+                <?php endif; ?>
 				<form id="customers" action="customers" method="post" autocomplete="off">
 					<table class="customer-table">
                         <thead>
