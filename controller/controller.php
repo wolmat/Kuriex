18ba1b3d6a37df3d419166c97d2316322ee3c473
davpal <paluchdd@gmail.com>
2015-06-22 00:16:48 +0200

    Delete methods from abstract controller

--- controller/controller.php
+++ controller/controller.php
@@ -1,67 +1,7 @@
 <?php
 
-abstract class Controller{
- 
-    /**
-     * Przekierowanie na konkretny adres URL
-     * @param adres url
-     */
+abstract class Controller {
     public function redirect($url) {
 		header("location:".$url);
     }
-	
-    /**
-     * Wczytywanie konkretnego widoku
-     * @param string $name nazwa klasy
-     * @param string $path ścieżka do pliku z klasą
-     * @return object
-     */
-    public function loadView($name, $path='view/') {
-		$path=$path.$name.'.php';
-        $name=$name.'View';
-        try {
-            if(is_file($path)) {
-                require $path;
-                $ob=new $name();
-            } else {
-                throw new Exception('Nie znaleziono '.$name.' w '.$path);
-            }
-        }
-        catch(Exception $e) {
-            echo $e->getMessage().'<br />
-                File: '.$e->getFile().'<br />
-                Code line: '.$e->getLine().'<br />
-                Trace: '.$e->getTraceAsString();
-            exit;
-        }
-        return $ob;
- 
-    }
-    /**
-     * Wczytywanie modelu
-     * @param string $name nazwa klasy
-     * @param string $path ścieżka do pliku z klasą
-     * @return object
-     */
-    public function loadModel($name, $path='model/') {
-		$path=$path.$name.'.php';
-        $name=$name.'Model';
-        try {
-            if(is_file($path)) {
-                require $path;
-                $ob=new $name();
-            } else {
-                throw new Exception('Nie znaleziono modelu '.$name.' w '.$path);
-            }
-        }
-        catch(Exception $e) {
-            echo $e->getMessage().'<br />
-                File: '.$e->getFile().'<br />
-                Code line: '.$e->getLine().'<br />
-                Trace: '.$e->getTraceAsString();
-            exit;
-        }
-        return $ob;
-    }
-
 }
