84aa9696795b8fb58ae0f69b00563c2d6db9ce36
krzyurb <krzyurb@gmail.com>
2015-06-23 17:50:23 +0200

    Change link in customer header to yourAccount

--- templates/customer/header.php
+++ templates/customer/header.php
@@ -1,7 +1,7 @@
 <div class="container">
   <h5>Zalogowany jako <span class="email"><?php echo $_SESSION['user']; ?></span></h5>
   <nav id="admin-nav">
-    <li><a href = "login">Twoje konto</a></li>
+    <li><a href = "yourAccount">Twoje konto</a></li>
     <li><a href = "yourOrders">Zlecenia</a></li>
     <li><a href = "logout">Wyloguj się</a></li>
   </nav>
